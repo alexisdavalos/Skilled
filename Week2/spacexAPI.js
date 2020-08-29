30dd0b24a138c441202a24a540574f015997ecba
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-08-29 10:33:49 +0200

    updated spacexApi.js

--- Week2/spacexAPI.js
+++ Week2/spacexAPI.js
@@ -24,3 +24,9 @@ const fetchData = async () => {
 };
 
 fetchData();
+
+// Assignment Instructions:
+
+//Create a program that will pull data from SpaceX api 'https://api.spacexdata.com/v3/history’ and display wikipedia link for each history event that comes back.
+//Wikipedia link is stored in the links object. Do not print null results.
+//Please implement using regular promises as well as using async/await syntax.
