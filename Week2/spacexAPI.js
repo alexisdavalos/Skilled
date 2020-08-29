e133355ba33c341a573375d1a16d9d79863d20ad
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-08-29 11:48:40 +0200

    added err catch to spacex api

--- Week2/spacexAPI.js
+++ Week2/spacexAPI.js
@@ -5,16 +5,20 @@ let spaceXData = [];
 // O(n) time | O(n) space
 const fetchData = async () => {
   // makes http request to spacex api
-  await axios.get("https://api.spacexdata.com/v3/history").then((res) => {
-    // loop through res object data
-    res.data.forEach((element) => {
-      // check element wikipedia link is not null
-      if (element.links.wikipedia !== null) {
-        // push link to arr
-        spaceXData.push(element.links.wikipedia);
-      }
-    });
-  });
+  await axios
+    .get("https://api.spacexdata.com/v3/history")
+    .then((res) => {
+      // loop through res object data
+      res.data.forEach((element) => {
+        // check element wikipedia link is not null
+        if (element.links.wikipedia !== null) {
+          // push link to arr
+          spaceXData.push(element.links.wikipedia);
+        }
+      });
+    })
+    // catches error and logs it
+    .catch((err) => console.log(err));
   // loop through spacex data and print each element
   if (spaceXData.length !== 0) {
     spaceXData.forEach((element) => {
